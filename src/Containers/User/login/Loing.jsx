2fddf0dfc52db94c1b7e4f4360f619b0382401a0
Devng Langhanoja <dclanghnoja@gmail.com>
2021-05-26 14:39:07 +0530

    Add login page to user side

---
+++ src/Containers/User/login/Loing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loing = () => {
+    return (
+        <div>
+            User login file
+        </div>
+    )
+}
+
+export default Loing
---
+++ src/Containers/admin/profile/profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const profile = () => {
+    return (
+        <div>
+            admin Proflie page
+        </div>
+    )
+}
+
+export default profile
