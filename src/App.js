6442bf6d2e93e15c2cdbcd66a96f29e4447cdf1e
Devng Langhanoja <dclanghnoja@gmail.com>
2021-05-26 12:58:09 +0530

    remove mdb react from template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
-import 'mdbreact/dist/css/mdb.css';
+
 function App() {
   return (
     <div>
