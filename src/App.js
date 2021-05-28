bddf430f8f13ed4ad7848f61a9995b122996b2c5
Husen Lokhandwala <coder.husen@gmail.com>
2021-05-28 16:40:30 +0530

    login route fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
           path='/user'
           render={({ match: { url } }) => (
             <>
-              <Route exact path={`${url}`} 
+              <Route path={`${url}`} 
                 render={({ match:{url} })=>(
                   <UserHome  url = {url}/>
                 )}
